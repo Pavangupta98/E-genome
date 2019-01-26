69607f57e38bc8f084fb1b41c512650c4f0094ba
iamsaurabh01 <saurabh.dewangan01@gmail.com>
2019-01-26 14:58:00 +0530

    commit before submission

--- E_GenomeServer/urls.py
+++ E_GenomeServer/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
 from news.views import news_feed
+from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
